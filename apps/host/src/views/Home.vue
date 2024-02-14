3eb4cb3319895bd8ab27d72ca5854e9ed871438f
Mirac <miracerdin@gmail.com>
2024-02-14 16:09:04 +0300

    refactor: dark mode default

--- apps/host/src/views/Home.vue
+++ apps/host/src/views/Home.vue
@@ -10,37 +10,25 @@ import { useToast } from "primevue/usetoast";
 const toast = useToast();
 
 const greet = () => {
-  toast.add({
-    severity: "success",
-    summary: "PrimeTime",
-    detail: modelValue.value,
-  });
+    toast.add({
+        severity: "success",
+        summary: "PrimeTime",
+        detail: modelValue.value,
+    });
 };
 </script>
 
 <template>
-  <main
-    class="flex justify-center bg-surface-0 dark:bg-surface-800 border-b border-surface-200 dark:border-surface-700 text-surface-900 dark:text-surface-0/80"
-  >
-    <Toast></Toast>
-    <span class="p-float-label">
-      <InputText
-        type="text"
-        v-model="modelValue"
-        size="small"
-        placeholder="Small"
-      />
-      <label for="username" class="bg-white dark:bg-black">Username</label>
-    </span>
-    <div class="w-1.5">
-      <Button
-        label="user"
-        rounded
-        @click="greet"
-        icon="pi pi-user"
-        class="text-black"
-        size="large"
-      />
-    </div>
-  </main>
+    <main
+        class="flex justify-center border-b border-surface-200 dark:border-surface-700 text-surface-900 dark:text-surface-0/80"
+    >
+        <Toast></Toast>
+        <span class="">
+            <InputText type="text" v-model="modelValue" size="small" placeholder="Small" />
+            <label for="username">Username</label>
+        </span>
+        <div class="w-1.5">
+            <Button label="user" rounded @click="greet" icon="pi pi-user" size="large" />
+        </div>
+    </main>
 </template>
